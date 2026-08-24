d5b373e5e67391ee70b014c7533152db336c1826
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-08-24 18:28:06 -0300

    Delete DoorsOS directory

--- DoorsOS/interrupts/idt.h
+++
@@ -1,9 +0,0 @@
-struct idt_entry {
-    unsigned short base_low;
-    unsigned short sel;
-    unsigned char zero;
-    unsigned char flags;
-    unsigned short base_high;
-} __attribute__((packed));
-
-void set_idt_gate(int n, unsigned long handler);
